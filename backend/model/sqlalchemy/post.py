b40d5e1c8517b13dbb88cc12dc5738a609396009
Rodrigo Ladvocat <rrlc@cin.ufpe.br>
2025-02-16 23:35:15 -0300

    fix: fixing the default timezone attribute

--- backend/model/sqlalchemy/post.py
+++ backend/model/sqlalchemy/post.py
@@ -6,10 +6,12 @@ from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 
 from datetime import datetime as dt
+from datetime import timezone
+
 
 class Post(Base):
     __tablename__ = "posts"
     id: Mapped[int] = mapped_column(primary_key=True)
     user_id: Mapped[int] = mapped_column(ForeignKey("user_account.id"))
     text: Mapped[str] = mapped_column(String(280), nullable=False)
-    date_time: Mapped[dt] = mapped_column(DateTime, default=dt.now(dt.timezone.utc), nullable=False)
+    date_time: Mapped[dt] = mapped_column(DateTime, default=dt.now(timezone.utc), nullable=False)
