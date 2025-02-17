30a78a02151f49f6b42fff122dfba532a245509a
Rodrigo Ladvocat <rrlc@cin.ufpe.br>
2025-02-16 23:21:11 -0300

    fix: fixing user object and follow tests + setting engine echo to False

--- backend/model/pydantic/user.py
+++ backend/model/pydantic/user.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel
 class User(BaseModel):
     id: int
     user_name: str
-    profile_img_path: str
+    profile_img_path: Optional[str] = None
     password: Optional[str] = None
     email: Optional[str] = None
     is_private: Optional[bool] = None
