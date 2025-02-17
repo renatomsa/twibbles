872d86a42b173722bccda3d799ab0f96f4857e17
Renato Serrano <renatom.sandrade@gmail.com>
2025-02-17 03:02:40 -0300

    fix: fix comment routes names

--- backend/src/api/comment.py
+++ backend/src/api/comment.py
@@ -5,12 +5,12 @@ from model.pydantic.comment import CommentCreate
 
 api = APIRouter()
 
-@api.get("/{post_id}/comments", response_model=HttpResponseModel)
+@api.get("/{post_id}", response_model=HttpResponseModel)
 def get_comments_for_post(post_id: int):
     response = comment_service.get_comments_by_post_id(post_id)
     return response
 
-@api.post("/{user_id}/comment/{post_id}", response_model=HttpResponseModel)
+@api.post("/user/{user_id}/post/{post_id}", response_model=HttpResponseModel)
 def create_comment_for_post(
     user_id: int,
     post_id: int,
@@ -19,7 +19,7 @@ def create_comment_for_post(
     response = comment_service.create_comment(user_id, post_id, comment_in)
     return response
 
-@api.delete("/{user_id}/comment/{comment_id}", response_model=HttpResponseModel)
+@api.delete("/user/{user_id}/comment/{comment_id}", response_model=HttpResponseModel)
 def delete_comment(user_id: int, comment_id: int):
     response = comment_service.delete_comment(user_id, comment_id)
     return response
