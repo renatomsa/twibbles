6d3c1f26b94505e6018117da1444e4887abc6cc2
João Henrique <joaohl19@users.noreply.github.com>
2025-02-16 22:52:30 -0300

    feat : implements posts model and creating posts routes

---
+++ backend/model/sqlalchemy/post.py
@@ -0,0 +1,15 @@
+from sqlalchemy import String, DateTime
+from sqlalchemy import ForeignKey
+from model.sqlalchemy.base import Base
+
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+
+from datetime import datetime as dt
+
+class Post(Base):
+    __tablename__ = "posts"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user_account.id"))
+    text: Mapped[str] = mapped_column(String(280), nullable=False)
+    date_time: Mapped[dt] = mapped_column(DateTime, default=dt.now(dt.timezone.utc), nullable=False)
