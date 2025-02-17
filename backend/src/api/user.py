cfc08eb878b247f143b1c6272ecb13816787e6a9
Rodrigo Ladvocat <rrlc@cin.ufpe.br>
2025-02-17 15:18:52 -0300

    feat: adding the delete user service and route

--- backend/src/api/user.py
+++ backend/src/api/user.py
@@ -10,12 +10,20 @@ def get_user_by_id(user_id: int):
     get_user_response = user_service.get_user(user_id)
     return get_user_response
 
+
 @api.get("/get_users_by_substring/{substring}", response_model=HttpResponseModel)
 def get_users_by_substring(substring: str):
     get_user_response = user_service.get_users_by_substring(substring)
     return get_user_response
 
+
 @api.patch("/update_user_privacy/{user_id}/{is_private}", response_model=HttpResponseModel)
 def update_user_privacy(user_id: int, is_private: bool):
     update_user_response = user_service.update_user_privacy(user_id, is_private)
     return update_user_response
+
+
+@api.delete("/delete_user/{user_id}", response_model=HttpResponseModel)
+def delete_user(user_id: int):
+    delete_user_response = user_service.delete_user(user_id)
+    return delete_user_response
